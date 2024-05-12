1cd2af4f3af609a42598eb6e408c347a79c4330b
OlegMartynov <olegmms@gmail.com>
2024-05-12 11:25:55 +0300

    feat: capitalizeWords

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,8 @@
 public class App {
+    public static void main(String[] args) {
+        var text = "hello hexlet k fffffff gd hello";
+        System.out.println(MyStringUtils.capitalizeWords(text)); // "Hello Hexlet"
+    }
+
+
 }
