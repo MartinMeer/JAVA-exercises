b30a2414077f91f4cf9fea2d41109e7bfc1d060c
OlegMartynov <olegmms@gmail.com>
2024-05-12 19:22:36 +0300

    feat: makeCensored

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,40 @@
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.StringJoiner;
+
+/*Реализуйте публичный статический метод makeCensored(),
+который заменяет каждое вхождение указанного слова (из списка переданных) в предложении
+на последовательность $#%! и возвращает полученную строку. Аргументы:
+Текст
+Массив стоп-слов
+Словом считается любая непрерывная последовательность символов, включая любые спецсимволы (без пробелов).*/
 public class App {
     public static void main(String[] args) {
-        var text = "hello hexlet k fffffff gd hello";
-        System.out.println(MyStringUtils.capitalizeWords(text)); // "Hello Hexlet"
+        var text1 = "When you play the game of thrones, you win or you die";
+        String[] stopWords1 = {"die", "play"};
+        var result1 = App.makeCensored(text1, stopWords1);
+        var text2 = "chicken chicken? chicken! chicken";
+        String[] stopWords2 = {"?", "chicken"};
+        var result2 = App.makeCensored(text2, stopWords2);
+        System.out.println(result2);
+// => "$#%! chicken? chicken! $#%!"
+// => "When you $#%! the game of thrones, you win or you $#%!"
+    }
+
+    public static String makeCensored(String text, String[] stopWords) {
+        String[] words = text.split(" ");
+        StringJoiner censoredString = new StringJoiner(" ");
+        String replacementWord = "$#%!";
+        for (int i = 0; i < words.length; i++) {
+            if (ArrayUtils.contains(stopWords, words[i])) {
+                words[i] = replacementWord;
+                censoredString.add(words[i]);
+            } else {
+                censoredString.add(words[i]);
+            }
+        }
+        return censoredString.toString();
     }
 
 
