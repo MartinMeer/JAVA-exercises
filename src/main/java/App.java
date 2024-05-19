e3f7c6a3fed912fac87179abe00ec7e94d4ca469
OlegMartynov <olegmms@gmail.com>
2024-05-19 14:43:47 +0300

    feat: countUniqChars v2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,29 +4,29 @@
 Например, в строке "yy" используется всего один символ — 'y'. А в строке "111yya!" — используется четыре символа: '1', 'y', 'a' и '!'.*/
 public class App {
     public static void main(String[] args) {
-        String str = "yy";
+        String str = "You know nothing Jon Snow";
         System.out.println(countUniqChars(str));
     }
 
     public static int countUniqChars(String str) {
         char[] chars = str.toCharArray();
-        int countUniqChars = 0;
+        int uniqChars = 0;
         int counter = 0;
         for (char givenChar : chars) {
-            int entry = 0;
-            for (char aChar : chars) {
-                if (givenChar == aChar) {
-                    entry++;
+            int entries = 0;
+            for (char comparedChar : chars) {
+                if (givenChar == comparedChar) {
+                    entries++;
                     chars[counter] = 0;
                 }
             }
-            if (entry < 2) {
-                countOf++;
-            } else if (entry == 1) {
-                countOf++;
+            if (entries < 2) {
+                uniqChars++;
+            } else if (entries == 1) {
+                uniqChars++;
             }
             counter++;
         }
-        return countUniqChars;
+        return uniqChars;
     }
 }
