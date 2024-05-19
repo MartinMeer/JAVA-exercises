de8c521b555916709595a6b61bf0888183574322
OlegMartynov <olegmms@gmail.com>
2024-05-19 13:17:45 +0300

    feat: bubbleSort v01

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,32 +1,30 @@
+import java.util.Arrays;
 
-/*Реализуйте публичный статический метод countUniqChars(),
-который принимает на вход строку и считает, сколько символов (без учёта повторяющихся символов) использовано в этой строке.
-Например, в строке "yy" используется всего один символ — 'y'. А в строке "111yya!" — используется четыре символа: '1', 'y', 'a' и '!'.*/
+/*Алгоритм состоит из повторяющихся проходов по сортируемому массиву.
+За каждый проход элементы последовательно сравниваются попарно
+и, если порядок в паре неверный, выполняется обмен элементов.
+Проходы по массиву повторяются N-1 раз
+или до тех пор,пока на очередном проходе не окажется, что обмены больше не нужны, что означает — массив отсортирован.
+При каждом проходе алгоритма по внутреннему циклу,
+очередной наибольший элемент массива ставится на свое место в конце массива рядом с предыдущим «наибольшим элементом»,
+а наименьший элемент перемещается на одну позицию к началу массива («всплывает» до нужной позиции, как пузырек в воде. Отсюда и название алгоритма).*/
 public class App {
     public static void main(String[] args) {
-        String str = "yy";
-        System.out.println(countUniqChars(str));
+        int[] numbers = {5, 8 ,2, 4, 7, 1, 6, 9, 3};
+        System.out.println(Arrays.toString(bubbleSort(numbers)));
     }
 
-    public static int countUniqChars(String str) {
-        char[] chars = str.toCharArray();
-        int countUniqChars = 0;
-        int counter = 0;
-        for (char givenChar : chars) {
-            int entry = 0;
-            for (char aChar : chars) {
-                if (givenChar == aChar) {
-                    entry++;
-                    chars[counter] = 0;
+    public static int[] bubbleSort(int[] numbers) {
+        int[] sortedNumbers = new int[numbers.length];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    sortedNumbers[i] = numbers[i + 1];
+                    sortedNumbers[i + 1] = numbers[i];
+                } else {
+                    sortedNumbers[i] = numbers[i];
+                    sortedNumbers[i + 1] = numbers[i + 1];
                 }
             }
-            if (entry < 2) {
-                countOf++;
-            } else if (entry == 1) {
-                countOf++;
-            }
-            counter++;
-        }
-        return countUniqChars;
+        return sortedNumbers;
     }
 }
