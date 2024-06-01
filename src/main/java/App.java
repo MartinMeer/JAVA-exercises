bd0f320610104546cfa0c8459eef2302b9bccf5b
OlegMartynov <olegmms@gmail.com>
2024-06-01 11:11:37 +0300

    feat: Book

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 /**/
 public class App {
     public static void main(String[] args) {
-        var textInput = " h h  h";
-        System.out.println(TextUtils.getWordsCount(textInput));
+    Book book = new Book("Firs Folio", "William Shakespeare", 1623);
+        System.out.println(book.toString());
+
     }
 }
