e739bfc8e2f74d8c55b63e9ef2397015423c884f
OlegMartynov <olegmms@gmail.com>
2024-06-02 19:27:21 +0300

    feat: Override equals() & hashcode()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,11 @@
-import org.apache.commons.lang3.ArrayUtils;
-
-import java.util.Arrays;
-
 /**/
 public class App {
     public static void main(String[] args) {
-    Book book = new Book("Firs Folio", "William Shakespeare", 1623);
-        System.out.println(book.toString());
+    User user1 = new User("Oleg", 45);
+    User user2 = new User("Marina", 35);
+    var user3 = new User("Oleg", 45);
+    User user4 = user1;
+        System.out.println(user4.hashCode());
 
     }
 }
