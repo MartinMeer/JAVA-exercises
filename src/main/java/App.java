b1e511cd8c2615307b30309314d45e05788fb091
OlegMartynov <olegmms@gmail.com>
2024-06-16 20:16:18 +0300

    feat:  Lambda 03 compute

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,40 +3,42 @@ import org.apache.commons.lang3.ArrayUtils;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
-/**/
+/*публичный статический метод countNumbers(),
+который принимает в качестве параметра список List целых чисел
+и подсчитывает количество положительных, отрицательных чисел и нулей в этом списке.
+Метод должен вернуть Map, где ключом будет строка - тип числа ("positive", "negative" или "zero"),
+а значением - количество чисел данного типа*/
+
 public class App {
     public static void main(String[] args) {
-        List<User> users = List.of(
-                /*new User("Salli", LocalDate.of(1990, 12, 15)),
-                new User("Gawen", LocalDate.of(2002, 10, 23)),
-                new User("Emmalee", LocalDate.of(1992, 9, 16)),
-                new User("Stacee", LocalDate.of(1974, 10, 25)),
-                new User("Lydia", LocalDate.of(1987, 5, 10)),
-                new User("Annabal", LocalDate.of(1970, 3, 22)),
-                new User("Tymon", LocalDate.of(2004, 8, 12)),
-                new User("Evania", LocalDate.of(1973, 4, 3))*/
-        );
-        System.out.println(getOldest(users));
+        var numbers1 = List.of(-2, -3, 4, 3, 8, 0, 0, 0, 0);
+        Map<String, Integer> result1 = App.countNumbers(numbers1);
+        System.out.println(result1);
+    } // => {"negative"=2, "positive"=3, "zero"=1}
 
-    }
-    public static List<User> getOldest(List<User> users) {
-        if (users.isEmpty()) {
-            return List.of();
-        }
-        ArrayList<User> oldestUsers= new ArrayList<>(users);
-        oldestUsers.sort((v1, v2) -> v1.getBirthday().compareTo(v2.getBirthday()));
-        return List.of(oldestUsers.get(0));
+    public static Map<String, Integer> countNumbers(List<Integer> numbers) {
+        Map<String, Integer> result = new HashMap<>(Map.of("zero", 0, "positive", 0, "negative", 0));
+        numbers.forEach((number) -> {
+            var type = convertKey(number);
+            result.compute(type, (k, v) -> v + 1);
+        });
+
+
+        return result;
     }
 
-    public static List<User> getOldest(List<User> users, int number) {
-        if (users.isEmpty()) {
-            return List.of();
+    public static String convertKey(int number) {
+        if (number > 0) {
+            return "positive";
+        } else if (number < 0) {
+            return "negative";
         }
-        ArrayList<User> oldestUsers= new ArrayList<>(users);
-        oldestUsers.sort((v1, v2) -> v1.getBirthday().compareTo(v2.getBirthday()));
-        return oldestUsers.subList(0, number);
+        return "zero";
     }
 
 }
