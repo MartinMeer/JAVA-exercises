206342ddaa3f36c392c08399a325a3db8eddc302
OlegMartynov <olegmms@gmail.com>
2024-06-17 13:57:37 +0300

    feat:  Lambda 05 Method References

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,42 @@
-import org.apache.commons.lang3.ArrayUtils;
 
 
+/*В классе App реализуйте публичный статический метод sortBooks(),
+который принимает на вход список книг List<Book> и сортирует его по названию книги в обратном порядке.
+Метод должен вернуть новый список*/
 
-/*Создайте функциональный интерфейс BinaryOperation, который будет представлять бинарную операцию.
-Так как операция бинарная, единственный метод интерфейса должен принимать два параметра - целые числа и возвращать целое число*/
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        BinaryOperation add = (v, t) -> v + t;
-        System.out.println(calculate(a, b, add));
+        var books = new ArrayList<>(
+                List.of(
+                        new Book("Dubliners", "James Joyce"),
+                        new Book("Moby-Dick", "Herman Melville"),
+                        new Book("The Great Gatsby", "F. Scott Fitzgerald"),
+                        new Book("Crime and Punishment", "Fyodor Dostoevsky"),
+                        new Book("Don Quixote", "Miguel de Cervantes")
+                )
+        );
+        System.out.println(sortBooks(books));
+
+        /*      books.get(2),
+                books.get(1),
+                books.get(0),
+                books.get(4),
+                books.get(3)*/
+
+    }
+
+    public static List<Book> sortBooks(List<Book> books) {
+        ArrayList<Book> sortedBooks = new ArrayList<>(books);
+        //Comparator<Book> forTitle = Comparator.comparing(Book::getTitle);
+        sortedBooks.sort(Comparator.comparing(Book::getTitle).reversed());
+        return sortedBooks;
     }
 
 
 
-    public static Integer calculate(Integer a, Integer b, BinaryOperation operation) {
-        return operation.operate(a, b);
-    }
 
 }
