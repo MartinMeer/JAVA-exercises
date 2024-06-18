70ffa83df9f668adaad22079f6e7cbda157713ba
OlegMartynov <olegmms@gmail.com>
2024-06-18 17:26:52 +0300

    feat: 02 make my own generic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,24 +4,25 @@
 который принимает на вход список книг List<Book> и сортирует его по названию книги в обратном порядке.
 Метод должен вернуть новый список*/
 
-import java.time.LocalDate;
+
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class App {
     public static void main(String[] args) {
+        var tr = new SimpleTriple("str", 1, true);
 
-        Box value1 = new Box("str");
-        Box value2 = new Box(1);
-        Box value3 = new Box(true);
-        Box value4 = new Box('a');
-        User user = new User("John", LocalDate.of(2000, 1, 1));
-        Box value5 = new Box(user);
+        tr.getLeft(); // str
+        tr.getRight(); // true
+        tr.getMiddle(); // 1
 
-        List<Box> list = List.of(value1, value2, value3, value4, value5);
+        System.out.println(tr.getLeft());
+    }
 
-        System.out.println();
-
+    public static List<Integer> duplicate(List<Integer> numbers) {
+        var duplicated = new ArrayList<Integer>(numbers);
+        duplicated.replaceAll((a) -> a * 2);
+        return duplicated;
     }
 }
---
+++ src/main/java/Triple.java
@@ -0,0 +1,15 @@
+public interface Triple<L, M, R> {
+    public void setLeft(L left);
+    public void setMiddle(M middle);
+
+    public void setRight(R right);
+
+    public L getLeft();
+    public M getMiddle();
+    public R getRight();
+
+
+
+
+
+}
