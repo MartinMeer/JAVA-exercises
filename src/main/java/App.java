ff32915dbc24b2f9a175a96c5bdb5a819b65191c
OlegMartynov <olegmms@gmail.com>
2024-06-18 15:32:10 +0300

    feat: 01 Box class contains Object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,36 +11,17 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        User user1 = new User("Sam", LocalDate.of(2000, 1, 10));
-        User user2 = new User("Bill", LocalDate.of(2000, 1, 10));
-        User user3 = new User("Eva", LocalDate.of(2000, 1, 10));
-        User user4 = new User("John", LocalDate.of(2000, 1, 10));
-        User user5 = new User("Mary", LocalDate.of(2000, 1, 10));
-        User user6 = new User("Ann", LocalDate.of(2000, 1, 10));
-        user1.setFriends(user2);
-        user1.setFriends(user2);
-        user1.setFriends(user3);
-        user2.setFriends(user1);
-        List<User> users = List.of(user1, user2, user3, user4, user5, user6);
-        List<User> usersWithoutFriends = CollectionsUtils.filter(users, (user) -> !user.getFriends().isEmpty());
-        System.out.println(usersWithoutFriends);
 
+        Box value1 = new Box("str");
+        Box value2 = new Box(1);
+        Box value3 = new Box(true);
+        Box value4 = new Box('a');
+        User user = new User("John", LocalDate.of(2000, 1, 1));
+        Box value5 = new Box(user);
 
+        List<Box> list = List.of(value1, value2, value3, value4, value5);
 
+        System.out.println();
 
     }
-
-    public static List<User> getUsersWithoutFriends(List<User> users) {
-        ArrayList<User> listOfUsers = new ArrayList<>();
-        users.forEach((user) -> {
-            if (user.getFriends().isEmpty()) {
-                listOfUsers.add(user);
-            };
-        });
-        return listOfUsers;
-    }
-
-
-
-
 }
--- src/main/java/CollectionsUtils.java
+++ src/main/java/CollectionsUtils.java
@@ -2,16 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class CollectionsUtils {
+public class CollectionsUtils<T> {
     private static final List<Object> filtered = new ArrayList<>();
-    public static List<User> filter (List<User> users, Predicate<User> fn) {
-        ArrayList<User> filteredList = new ArrayList<>();
-        for (User user : users) {
+    public List<Object> filter(List<Object> users, Predicate<Object> fn) {
+        ArrayList<Object> filteredList = new ArrayList<>();
+        for (Object user : users) {
             if (fn.test(user)) {
                 filteredList.add(user);
             }
         }
-
-        return filteredList;
+    return filteredList;
     }
 }
