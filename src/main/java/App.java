1c9acf734ffdaf09ea69bea3997481035f30968f
OlegMartynov <olegmms@gmail.com>
2024-06-20 14:17:21 +0300

    feat: 02 stream.reduce()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,53 @@
-/*В классе App реализуйте публичный статический метод sortWords(), который сортирует список List строк.
-Метод должен вернуть новый список, строки в котором отсортированы в алфавитном порядке. Используйте в реализации стримы*/
+/*публичный статический метод normalize(), который будет выполнять нормализацию.
+Метод принимает в качестве параметра список List email-адресов и возвращает новый список, в котором каждый email нормализован.
+Нормализация заключается в удалении концевых пробелов и приведении адреса к нижнему регистру*/
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
+        var people = new ArrayList(
+                List.of(
+                        new Person("John", 17),
+                        new Person("Anna", 24),
+                        new Person("Alex", 57),
+                        new Person("Jun", 32)
+                )
+        );
 
-        var fruits = List.of("lemon", "apple", "banana");
-        var result = App.sortWords(fruits);
-        System.out.println(result); // => [apple, banana, lemon]
+        System.out.println(App.getAverageAge(people)); // 32.5
+
+
+
     }
-    public static List<String> sortWords(List<String> words) {
-        var stream = words.stream()
-                .sorted()
-                .toList();
-        return stream;
+    /*В классе App реализуйте публичный статический метод getAverageAge(),
+    который принимает в качестве параметра группу людей — список List<Person>.
+    Метод должен вернуть средний возраст людей в нашей группе в виде числа типа Double.
+    Если передан пустой список, метод должен вернуть null*/
+
+    public static Double getAverageAge(List<Person> persons) {
+        if (persons.isEmpty()) {
+            return null;
+        }
+        var sumStream = persons.stream()
+                .reduce(0.0, (sum, person) -> sum + person.getAge(), Double::sum);
+        //double avg = sumStream.doubleValue();
+        return sumStream / persons.size();
+
+
+
+
+
+
     }
+
+
+
+
+
 }
