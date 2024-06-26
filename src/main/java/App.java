3b08229d1b0c4c44992f25a8681b64010a18a414
OlegMartynov <olegmms@gmail.com>
2024-06-26 10:46:17 +0300

    feat: 07 secondBiggest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,50 +1,25 @@
-
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 
 public class App {
 
     public static void main(String[] args) {
-
-        var users = List.of(
-                        new User(Long.valueOf(1), "John"),
-                        new User(Long.valueOf(2), "Anna"),
-                        new User(Long.valueOf(3), "Alex")
-                        );
-
-        var user = App.findUserById(users, 1L);
-        //System.out.println(user.getName()); // John
-
-// Пользователя с таким id нет
-        App.findUserById(users, 10L); // Error
+        var numbers = List.of(3, -5, 6, 1, 0, -2, 10);
+        var numbers1 = List.of(-100, 1, -12, -1, -56, -34);
+        System.out.println(getSecondBiggest(numbers1));
     }
-    /*В классе App реализуйте публичный статический метод findUserById(), который ищет пользователя в списке по идентификатору.
-Метод принимает в качестве параметра список пользователей List<User> и идентификатор пользователя, число типа long.
-Он должен вернуть пользователя с таким идентификатором, если он есть в списке.
-А если такого пользователя нет, выбросить исключение RuntimeException с сообщением User not found*/
-    public static User findUserById(List<User> users, long id) {
-        var userById = users.stream()
-                .filter(user -> user.getId() == id)
-                .findFirst();
-        return userById.orElseThrow(() -> new RuntimeException("User not found"));
+    /*В классе App реализуйте публичный статический метод getSecondBiggest(), который принимает в качестве параметра список целых чисел List<Integer>. \
+    Метод должен вернуть второе по величине число из списка.
+    Если найти такое число не представляется возможным (например, список пуст или в нем только один элемент), метод должен вернуть null*/
+
+    public static Integer getSecondBiggest(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted((a, b) -> (Integer.compare(b, a)))
+                .skip(1)
+                .findFirst()
+                .orElse(null);
     }
-
-
-
-
-
 }
 
 
