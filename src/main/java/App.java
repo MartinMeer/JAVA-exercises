1eb019c210bfdf938234ee60c92bb0052cf7c9f3
OlegMartynov <olegmms@gmail.com>
2024-06-26 10:56:37 +0300

    feat: getEvenNumbers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,28 @@
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) {
-        var numbers = List.of(3, -5, 6, 1, 0, -2, 10);
-        var numbers1 = List.of(-100, 1, -12, -1, -56, -34);
-        System.out.println(getSecondBiggest(numbers1));
+
+        System.out.println(getEvenNumbers(10, 1));
     }
     /*В классе App реализуйте публичный статический метод getSecondBiggest(), который принимает в качестве параметра список целых чисел List<Integer>. \
     Метод должен вернуть второе по величине число из списка.
     Если найти такое число не представляется возможным (например, список пуст или в нем только один элемент), метод должен вернуть null*/
 
-    public static Integer getSecondBiggest(List<Integer> numbers) {
-        return numbers.stream()
-                .sorted((a, b) -> (Integer.compare(b, a)))
-                .skip(1)
-                .findFirst()
-                .orElse(null);
+    private static List<Integer> getEvenNumbers(int offset, int limit) {
+        return Stream.iterate(111, i -> i + 1)
+                .filter(i -> i % 2 == 0)
+                .skip(offset)
+                .limit(limit)
+                .collect(Collectors.toList());
     }
+
+
 }
 
 
